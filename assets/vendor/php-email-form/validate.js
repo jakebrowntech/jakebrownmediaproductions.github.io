ca6ad62053ad5dd175de651dec22e3c7ae4bad32
Jake Brown <64292750+thetechprofessor@users.noreply.github.com>
2020-10-15 11:33:06 -0400

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,129 +1,165 @@
-<script type="text/javascript">
-jQuery(document).ready(function(){
-	
-	$('#message').hide();
-	
-	// Add validation parts
-	$('#contact input[type=text], #contact input[type=number], #contact input[type=email], #contact input[type=url], #contact input[type=tel], #contact select, #contact textarea').each(function(){
-		$(this).after('<mark class="validate"></mark>');
-	});
-	
-	// Validate as you type
-	$('#name, #comments').focusout(function() {
-		if (!$(this).val()) 
-			$(this).addClass('error').parent().find('mark').removeClass('valid').addClass('error');
-		else 
-			$(this).removeClass('error').parent().find('mark').removeClass('error').addClass('valid');
-	});
-	$('#email').focusout(function() {
-		if (!$(this).val() || !isEmail($(this).val())) 
-			$(this).addClass('error').parent().find('mark').removeClass('valid').addClass('error');
-		else 
-			$(this).removeClass('error').parent().find('mark').removeClass('error').addClass('valid');
-	});
-	$('#website').focusout(function() {
-		var web = $(this).val();
-		if (web && web.indexOf("://") == -1) {
-			//$(this).addClass('error').parent().find('mark').removeClass('valid').addClass('error');
-			$(this).val('http://' + web);
-			$(this).removeClass('error').parent().find('mark').removeClass('error').addClass('valid');			
-		} else if (web)
-			$(this).removeClass('error').parent().find('mark').removeClass('error').addClass('valid');
-		else 
-			$(this).removeClass('error').parent().find('mark').removeClass('error').removeClass('valid');
-	});
-	$('#verify').focusout(function() {
-		var verify = $(this).val();
-		var verify_box = $(this);
-		if (!verify) 
-			$(this).addClass('error').parent().find('mark').removeClass('valid').addClass('error');
-		else {
-		
-			// Test verification code via ajax
-			$.ajax({
-				type: 'POST',
-				url: 'verify/ajax_check.php',
-				data: { verify: verify },
-				async: false,
-				success: function( data ) {
-					if (data=='success') {
-						$(verify_box).removeClass('error').parent().find('mark').removeClass('error').addClass('valid');
-					} else {
-						$(verify_box).addClass('error').parent().find('mark').removeClass('valid').addClass('error');
-					}
-				}
-			});
-		
-		}			
-	});
-	
-	$('#submit').click(function() {
-		$("#message").slideUp(200,function() {
-			$('#message').hide();
-		
-			// Kick in Validation
-			$('#name, #phone, #comments, #website, #verify, #email').triggerHandler("focusout");
-			
-			if ($('#contact mark.error').size()>0) {
-				$('#contact').effect('shake', { times:2 }, 75, function(){
-					$('#contact input.error:first, #contact textarea.error:first').focus();
-				});				
-				return false;
-			}
-			
-		});
-	});
-	
-	$('#contactform').submit(function(){
-		
-		if ($('#contact mark.error').size()>0) {
-			$('#contact').effect('shake', { times:2 }, 75);
-			return false;
-		}
-		
-		var action = $(this).attr('action');
-		
- 		$('#submit')
-			.after('<img src="assets/ajax-loader.gif" class="loader" />')
-			.attr('disabled','disabled');
-		
-		$.post(action, { 
-			name: $('#name').val(),
-			email: $('#email').val(),
-			subject: $('#subject').val(),
-			comments: $('#comments').val(),
-		},
-			function(data){
-				$('#message').html( data );
-				$('#message').slideDown();
-				$('#contactform img.loader').fadeOut('slow',function(){$(this).remove()});
-				$('#contactform #submit').attr('disabled',''); 
-				if(data.match('success') != null) $('#contactform').slideUp('slow');
-				
-			}
-		);
-		
-		return false; 
-	
-	});
-	
-	function isEmail(emailAddress) {
+/**
+* PHP Email Form Validation - v2.1
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+!(function($) {
+  "use strict";
 
-		var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-		
-		return pattern.test(emailAddress);
-	}
-	
-	function isNumeric(input) {
-    	return (input - 0) == input && input.length > 0;
-	}
-	
-});
-</script>
+  $('form.php-email-form').submit(function(e) {
+    e.preventDefault();
+    
+    var f = $(this).find('.form-group'),
+      ferror = false,
+      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-<head>
-    <meta http-equiv="Refresh" content="4; url='https://jakebrownmediaproductions.com'" />
-  </head>
-  <body>
-    <p> Your Message Has Been Sent. Thank you.</p>
-  </body>
+    f.children('input').each(function() { // run all inputs
+     
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'checked':
+            if (! i.is(':checked')) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            exp = new RegExp(exp);
+            if (!exp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validate').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    f.children('textarea').each(function() { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    if (ferror) return false;
+
+    var this_form = $(this);
+    var action = $(this).attr('action');
+
+    if( ! action ) {
+      this_form.find('.loading').slideUp();
+      this_form.find('.error-message').slideDown().html('The form action property is not set!');
+      return false;
+    }
+    
+    this_form.find('.sent-message').slideUp();
+    this_form.find('.error-message').slideUp();
+    this_form.find('.loading').slideDown();
+
+    if ( $(this).data('recaptcha-site-key') ) {
+      var recaptcha_site_key = $(this).data('recaptcha-site-key');
+      grecaptcha.ready(function() {
+        grecaptcha.execute(recaptcha_site_key, {action: 'php_email_form_submit'}).then(function(token) {
+          php_email_form_submit(this_form,action,this_form.serialize() + '&recaptcha-response=' + token);
+        });
+      });
+    } else {
+      php_email_form_submit(this_form,action,this_form.serialize());
+    }
+    
+    return true;
+  });
+
+  function php_email_form_submit(this_form, action, data) {
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: data,
+      timeout: 40000
+    }).done( function(msg){
+      if (msg.trim() == 'OK') {
+        this_form.find('.loading').slideUp();
+        this_form.find('.sent-message').slideDown();
+        this_form.find("input:not(input[type=submit]), textarea").val('');
+      } else {
+        this_form.find('.loading').slideUp();
+        if(!msg) {
+          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+        }
+        this_form.find('.error-message').slideDown().html(msg);
+      }
+    }).fail( function(data){
+      console.log(data);
+      var error_msg = "Form submission failed!<br>";
+      if(data.statusText || data.status) {
+        error_msg += 'Status:';
+        if(data.statusText) {
+          error_msg += ' ' + data.statusText;
+        }
+        if(data.status) {
+          error_msg += ' ' + data.status;
+        }
+        error_msg += '<br>';
+      }
+      if(data.responseText) {
+        error_msg += data.responseText;
+      }
+      this_form.find('.loading').slideUp();
+      this_form.find('.error-message').slideDown().html(error_msg);
+    });
+  }
+
+})(jQuery);
