fc7007b6b778f5db50efd417a3db06285e396d64
Jake Brown <64292750+thetechprofessor@users.noreply.github.com>
2020-10-15 10:07:01 -0400

    Update contactold.php

--- forms/contactold.php
+++ forms/contactold.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  *
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'jake@jakebrownmediaproductions.com',
-    'password' => 'ayljmniemgdpgqjk',
-    'port' => '465'
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'port' => ''
   );
-  *
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
